e7f1c51101e0eecb470dd418b7f7a5c674a11d97
devil982 <85802649+devil982@users.noreply.github.com>
2021-10-08 09:21:14 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -10,5 +10,7 @@ async def start(client, message):
             "Report Bugs 😊", url="https://t.me/pranthan_321")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b><b> i am a youtube downloader bot</b>\n/help for More info"
+                                   [Text or Emoji](https://telegra.ph/file/a93dd72c108a2ff0342fd.jpg
+)
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
