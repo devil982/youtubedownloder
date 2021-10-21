86223f561d1974f45bd24a25b16f577fb9b3853e
thomas shelby <85802649+devil982@users.noreply.github.com>
2021-10-21 21:49:00 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -12,4 +12,8 @@ async def start(client, message):
     welcomed = f"Hey <b>{message.from_user.first_name}</b><b> i am a youtube downloader bot</b>\n/help for More info"
                                   
     await message.reply_photo("https://telegra.ph/file/a93dd72c108a2ff0342fd.jpg")
+    message.reply_text("hi.. i am a youtube downloaderbot")
+     [InlineKeyboardButton(
+            "Report Bugs 😊", url="https://t.me/pranthan_321")]
+    ])
     raise StopPropagation
